47b2df3002969c3e1b92fd6fdcfe235a908cd9f4
Csaba Palfi <csaba@palfi.me>
2015-06-12 23:21:27 +0100

    refactor ui

--- index.js
+++ index.js
@@ -12,24 +12,32 @@ var client = new Riak.Client(riakNodes);
 var clientId = uuid.v4();
 var app = express();
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: ".hbs"
+}));
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
-    client.fetchValue({
-        bucket: 'clients',
-        key: clientId,
-        convertToJs: true
-    }, function(err, result) {
-        if (err) return res.render('index', {error: err});
+    getCounterValue(clientId, function(err, result) {
+        if (err) return res.render('error', {error: err});
+        res.render('index', {
+            clientId: clientId,
+            counter: result ? result.value.counter : 0
+        });
+    });
+});
+
+app.get('/counter', function (req, res) {
+    getCounterValue(clientId, function(err, result) {
+        if (err) return res.render('error', {error: err});
         var value = null;
-        if(result.isNotFound) {
+        if(!result) {
             value = { counter: 1 };
         } else {
-            var riakObject = result.values.shift();
-            var oldValue = riakObject.value.counter;
-            riakObject.setValue({counter: oldValue+1});
-            value = riakObject;
+            var oldValue = result.value.counter;
+            result.setValue({counter: oldValue+1});
+            value = result;
         }
         client.storeValue({
             bucket: 'clients',
@@ -38,9 +46,8 @@ app.get('/', function (req, res) {
             convertToJs: true,
             returnBody: true
         }, function(err, result) {
-            if (err) return res.render('index', {error: err});
-            var counter = result.values.shift().value.counter;
-            res.render('index', {clients: [{clientId: clientId, counterValue: counter}]});
+            if (err) return res.render('error', {error: err});
+            res.redirect('/');
         });
     });
 
@@ -52,30 +59,36 @@ app.get('/all', function (req, res) {
         stream: false
     },
     function (err, result) {
-        if(err) return res.end(err);
+        if (err) return res.render('error', {error: err});
         var results = {};
         async.mapSeries(result.keys.sort(),
             function (key, callback) {
-                client.fetchValue({
-                    bucket: 'clients',
-                    key: key,
-                    convertToJs: true
-                },
-                function (err, result) {
+                getCounterValue(key, function (err, result) {
                     if(err) return callback(err);
                     callback(null, {
                         clientId: key,
-                        counterValue: result.values.shift().value.counter
+                        counterValue: result.value.counter
                     });
                 });
         }, function (err, results) {
-            if(err) return res.end(err);
-            res.render('index', {clients: results});
+            if (err) return res.render('error', {error: err});
+            res.render('counters', {clients: results});
         });
 
     });
 
 });
+
+function getCounterValue (clientId, callback) {
+    client.fetchValue({
+        bucket: 'clients',
+        key: clientId,
+        convertToJs: true
+    }, function(err, result) {
+        if(err) return callback(err);
+        callback(null, result.values.shift());
+    });
+}
 
 var server = app.listen(port, function () {
     var gracefulExit = function () {
